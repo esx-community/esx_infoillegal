6f189b76cc316924f29fc89515c3dff86ad94290
Rockmar <33753269+rockmar1@users.noreply.github.com>
2017-12-05 01:04:47 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,33 @@
+Config					= {}
+Config.Locale			= 'fr'
+Config.MarkerSize   	= {x = 3.0, y = 3.0, z = 3.0}
+Config.openHours		= 12
+Config.closeHours		= 22
+Config.Blip 			= true
+Config.Hours 			= true
+Config.GPS 				= true
+Config.PriceWeedF 		= 2500
+Config.PriceWeedT		= 5000
+Config.PriceWeedR		= 6000
+
+Config.Zones = {
+	{x = 149.14811706543, y = 323.48037719727, z = 111.13859558105},
+}
+
+Config.WeedFarm = {
+	x = 1570.0228271484,
+	y = 6600.3955078125,
+	z = 12.613143920898,
+}
+
+Config.WeedTreatment = {
+	x = 91.061386108398,
+	y = 3750.0380859375,
+	z = 39.77326965332,
+}
+
+Config.WeedResell = {
+	x = -54.249694824219,
+	y = -1443.3666992188,
+	z = 31.068626403809,
+}
